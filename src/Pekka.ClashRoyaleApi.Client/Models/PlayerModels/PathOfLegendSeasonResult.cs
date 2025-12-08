13e540041b1cc04bdb47ace959e3fa9d1e8a8d37
m.dellaquila <m.dellaquila@opencluster.it>
2025-12-08 10:10:19 +0100

    fix errore di deserializzazione

--- src/Pekka.ClashRoyaleApi.Client/Models/PlayerModels/PathOfLegendSeasonResult.cs
+++ src/Pekka.ClashRoyaleApi.Client/Models/PlayerModels/PathOfLegendSeasonResult.cs
@@ -6,10 +6,10 @@ namespace Pekka.ClashRoyaleApi.Client.Models.PlayerModels
     [JsonObject(NamingStrategyType = typeof(CamelCaseNamingStrategy))]
     public class PathOfLegendSeasonResult
     {
-        public int Trophies { get; set; }
+        public int? Trophies { get; set; }
         
-        public int Rank { get; set; }
+        public int? Rank { get; set; }
         
-        public int LeagueNumber { get; set; }
+        public int? LeagueNumber { get; set; }
     }
 }
